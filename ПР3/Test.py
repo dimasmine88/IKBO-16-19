f7dc9a4add74f89d0b47d3a9f9f86992843f6c44
TheDavidGame <vlad.billI@bk.ru>
2021-10-19 00:15:00 +0300

    fix gorlo

--- ПР3/Test.py
+++ ПР3/Test.py
@@ -1,6 +1,7 @@
 import unittest
 import main
 
+
 class Test_Chto_Bolit(unittest.TestCase):
     def test_chto_bolit1(self):
         self.assertEqual(self.chto_bolit(1), "голова")
@@ -15,28 +16,39 @@ class Test_Chto_Bolit(unittest.TestCase):
 class Test_Grudi(unittest.TestCase):
     def test_grudi1(self):
         self.assertEqual(self.bolit_grudi("да"), "Обратись к кардиологу")
+
     def test_grudi2(self):
         self.assertEqual(self.bolit_grudi("нет", "да"), "Обратись к пульмологу")
+
     def test_grudi3(self):
         self.assertEqual(self.bolit_grudi("нет", "нет", "да"), "Сходите в травмпункт")
+
     def test_grudi4(self):
         self.assertEqual(self.bolit_grudi("нет", "нет", "нет"), "Симулянт")
+
 
 class Test_Gorlo(unittest.TestCase):
     def test_gorlo1(self):
         self.assertEqual(self.bolit_gorlo("да"), "Кашель есть?")
+
     def test_gorlo2(self):
         self.assertEqual(self.bolit_gorlo("да", "нет"), "Ангина")
+
     def test_gorlo3(self):
-        self.assertEqual(self.bolit_gorlo("да", "да","влажный"), "Бронхит")
+        self.assertEqual(self.bolit_gorlo("да", "да", "влажный"), "Бронхит")
+
     def test_gorlo4(self):
-        self.assertEqual(self.bolit_gorlo("да", "да","сухой"), "Тонзиллит")
-    def test_gorlo4(self):
+        self.assertEqual(self.bolit_gorlo("да", "да", "сухой"), "Тонзиллит")
+
+    def test_gorlo5(self):
         self.assertEqual(self.bolit_gorlo("нет", "да"), "Не напрягайте их в ближайшее время")
-    def test_gorlo4(self):
-        self.assertEqual(self.bolit_gorlo("нет", "нет","да"), "Обратись к ЛОР-врачу")
-    def test_gorlo4(self):
-        self.assertEqual(self.bolit_gorlo("нет", "нет","нет"), "Симулянт")
+
+    def test_gorlo6(self):
+        self.assertEqual(self.bolit_gorlo("нет", "нет", "да"), "Обратись к ЛОР-врачу")
+
+    def test_gorlo7(self):
+        self.assertEqual(self.bolit_gorlo("нет", "нет", "нет"), "Симулянт")
+
 
 if __name__ == '__main__':
     unittest.main()

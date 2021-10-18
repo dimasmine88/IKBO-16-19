93c3bb9f65df42b9e1a07d5225695e970287396d
dimasmine88 <dimasmine88@gamil.com>
2021-10-18 23:44:39 +0300

    Добавил тест метода класса выбора того что болит

--- ПР3/Test.py
+++ ПР3/Test.py
@@ -1,9 +1,15 @@
 import unittest
+import main
 
+class Test_Chto_Bolit(unittest.TestCase):
+    def test_chto_bolit(self):
+        self.assertEqual(self.chto_bolit(1), "голова")
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_chto_bolit(self):
+        self.assertEqual(self.chto_bolit(2), "грудь")
+
+    def test_chto_bolit(self):
+        self.assertEqual(self.chto_bolit(3), "горло")
 
 
 if __name__ == '__main__':
